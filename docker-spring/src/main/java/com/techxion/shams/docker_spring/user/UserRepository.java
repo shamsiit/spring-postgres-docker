29191645def76162fc9295863fdf63afa6b9a73a
shahriar <shahriar@moleculedata.com>
2025-03-07 16:48:13 +0600

    Dockerized basic spring crud app with postgres

---
+++ docker-spring/src/main/java/com/techxion/shams/docker_spring/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.techxion.shams.docker_spring.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
---
+++ docker-spring/src/test/java/com/techxion/shams/docker_spring/DockerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techxion.shams.docker_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
